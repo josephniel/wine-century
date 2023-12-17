731d35697489b8139fb7fd189c2c139f3546c881
Joseph Niel Tuazon <josephniel@users.noreply.github.com>
2023-12-17 13:14:17 +0800

    chore: Add jest test

--- backend/shared/config/api.ts
+++ backend/shared/config/api.ts
@@ -1,9 +1,15 @@
 export interface APIConfig {
   port: number;
+
+  privateKey: string;
+  publicKey: string;
 }
 
 export const loadAPIConfig = (): APIConfig => {
   return {
-    port: Number(process.env['PORT']) | 3030,
+    port: Number(process.env['API_PORT']) || 3030,
+
+    privateKey: process.env['API_PRIVATE_KEY'] || '',
+    publicKey: process.env['API_PUBLIC_KEY'] || '',
   }
 };
---
+++ backend/shared/config/index.spec.ts
@@ -0,0 +1,28 @@
+import { loadConfig } from ".";
+
+describe('Config', () => {
+  let oldEnv: NodeJS.ProcessEnv = {};
+  
+  beforeEach(() => {
+    oldEnv = process.env;
+
+    process.env['API_PORT'] = '9876';
+    process.env['API_PRIVATE_KEY'] = 'mock-private-key';
+    process.env['API_PUBLIC_KEY'] = 'mock-public-key';
+  });
+
+  afterEach(() => {
+    process.env = oldEnv;
+  })
+
+  it('should read from the environment variables', () => {
+    const config = loadConfig();
+    expect(config).toStrictEqual({
+      api: {
+        port: 9876,
+        privateKey: 'mock-private-key',
+        publicKey: 'mock-public-key',
+      },
+    });
+  });
+});
