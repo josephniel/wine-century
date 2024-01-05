a47bc16ca70b69cda44ea282463ba5386bee14cf
Joseph Niel Tuazon <josephniel@users.noreply.github.com>
2024-01-05 20:20:56 +0800

    chore: remove unused style

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
-import './index.scss';
-
 import reportWebVitals from './reportWebVitals';
 import router from './router';
 
