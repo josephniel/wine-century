39c4dee31a6d036e69ddd2f1a6b1eaf1304aa6a1
Joseph Niel Tuazon <josephnieltuazon@gmail.com>
2024-02-19 00:54:40 +0800

    fix: frontend envs

--- frontend/src/admin/api/config.ts
+++ frontend/src/admin/api/config.ts
@@ -1,3 +1,6 @@
 export default {
-  backend_url: process.env.BACKEND_HOST ?? 'http://localhost:3030'
+  backend_url:
+    process.env.NODE_ENV === 'production'
+      ? 'https://wine-century-backend-gqldhk5rva-as.a.run.app'
+      : 'http://localhost:3030'
 };
